558c4a0ee648f31d153e0a5e4d8c6f26c2718e36
Hiroshi Urabe <mail@torounit.com>
2015-04-06 12:23:56 +0900

    null じゃなくて、引数をそのままreturn.

--- rs-csv-media-addon.php
+++ rs-csv-media-addon.php
@@ -13,13 +13,18 @@ Domain Path: /languages
 
 add_filter( 'really_simple_csv_importer_class', 'really_simple_csv_importer_class');
 
-function really_simple_csv_importer_class() {
+function really_simple_csv_importer_class( $class ) {
 
-	if( class_exists('RS_CSV_Importer') and !class_exists('RS_CSV_Media') ) {
+	if( !class_exists('RS_CSV_Importer') ) {
+		return $class;
+	}
+
+	if( !class_exists('RS_CSV_Media') ) {
 		require_once dirname(__FILE__)."/class-rs_csv_media.php";
 		return 'RS_CSV_Media';
 	}
 
+	return $class;
 }
 
 
