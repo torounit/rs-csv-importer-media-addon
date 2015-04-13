da4230de0ecfca9f6f4cabf4183818837760441c
Hiroshi Urabe <mail@torounit.com>
2015-04-13 23:18:49 +0900

    require cache

--- rs-csv-importer-media-addon.php
+++ rs-csv-importer-media-addon.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: RS CSV Importer Media Add-On.
 Version: 0.1-alpha
-Description: Really Simple CSV Importer Add-on. Media's URL (Images, Documents... etc) in CSV, Download Media and Convert value url to attachment ID.
+Description: Really Simple CSV Importer Add-on. Media's URL (Images, Documents... etc) in CSV, Download Media and Convert url to attachment ID.
 Author: Toro_Unit
 Author URI: http://www.torounit.com
 Plugin URI: PLUGIN SITE HERE
@@ -19,12 +19,17 @@ function really_simple_csv_importer_class( $class ) {
 		return $class;
 	}
 
-	if ( ! class_exists( 'RS_CSV_Importer_Media' ) ) {
-		require_once dirname( __FILE__ ) . "/class-rs_csv_importer_media.php";
-	}
+	require_once dirname( __FILE__ ) . "/class-rs_csv_importer_media.php";
+	require_once dirname( __FILE__ ) . "/class-rs_csv_media_cache.php";
 
 	return 'RS_CSV_Importer_Media';
 
 }
 
+add_filter('really_simple_csv_importer_media_ext2type', 'really_simple_csv_importer_media_ext2type');
 
+function really_simple_csv_importer_media_ext2type( $types ) {
+	return array(
+		'image'       => array( 'gif',  'png',  'bmp',   'tif',  'tiff', 'ico' ),
+	);
+}
