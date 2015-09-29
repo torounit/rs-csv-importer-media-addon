d17cc6bdce055c392790f7ef1ed662d464884de7
Hiroshi Urabe <mail@torounit.com>
2015-09-30 01:26:07 +0900

    add tests

---
+++ tests/dependencies-array.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+	'really-simple-csv-importer' => array(
+		'include' => 'really-simple-csv-importer/rs-csv-importer.php',
+		'repo' => 'https://downloads.wordpress.org/plugin/really-simple-csv-importer.zip',
+	)
+);
